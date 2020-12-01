82a51feb55a1cca59d38bf13173fbc7c27d1ed3b
nicohj <nicoj98@gmail.com>
2020-12-01 12:41:29 +0100

    fetch loop + fonts imported

--- src/App.js
+++ src/App.js
@@ -2,20 +2,29 @@ import React, { useState, useEffect } from "react";
 
 import { get } from "./modules/rest";
 
-import Main from "./components/Main";
 import "./App.scss";
 
 function App() {
-  const [facts, setFacts] = useState([]);
-  useEffect(() => {
-    get(setFacts);
-  }, []);
-
-  return (
-    <div className="App">
-      <Main facts={facts}></Main>
-    </div>
-  );
+	const [facts, setFacts] = useState([]);
+	useEffect(() => {
+		get(setFacts);
+		const interval = setInterval(() => {
+			get(setFacts);
+		}, 2000);
+		return () => clearInterval(interval);
+	}, []);
+	console.log(facts);
+	return (
+		<div className="App">
+			{facts.bar !== undefined ? (
+				<div>
+					<h1>{facts.bar.name}</h1>
+					<p>{facts.bartenders[0].name}</p>
+					<p>{facts.bartenders[0].statusDetail}</p>
+				</div>
+			) : null}
+		</div>
+	);
 }
 
 export default App;
