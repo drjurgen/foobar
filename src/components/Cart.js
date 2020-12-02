466de251d9a214d3f6bb92737027307e5082c501
nicohj <nicoj98@gmail.com>
2020-12-02 20:27:20 +0100

    bottom card dialog styled

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,8 +2,20 @@ import React from "react"; // import React
 
 export default function Cart() {
 	return (
-		<div>
-			<p>cart here</p>
+		<div className="background-container">
+			<div className="mini-cart-container">
+				<div className="cart-info">
+					<div className="current-order">
+						<h3>Current order</h3>
+						<h3>?? DKK</h3>
+					</div>
+					<p>up to three beers shown here...</p>
+				</div>
+
+				<div className="open-cart">
+					<button className="open-cart-btn"></button>
+				</div>
+			</div>
 		</div>
 	);
 }
